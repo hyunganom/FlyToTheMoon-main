1e25517d255c8ea5fb80dce47bc875ffee5fade3
hyunjae <dlguswo0504@naver.com>
2025-02-16 15:01:57 +0900

    test

--- study/src/main/java/com/example/study/Controller/UserController.java
+++ study/src/main/java/com/example/study/Controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {// 1
+public class UserController {// 12
 
     @Autowired
     private UserService userService;
