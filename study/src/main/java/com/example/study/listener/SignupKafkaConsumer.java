d06697d5294a32bf545298aa82a4f746418b53e6
hyunjae <dlguswo0504@naver.com>
2025-03-08 16:10:06 +0900

    배포 수정

--- study/src/main/java/com/example/study/listener/SignupKafkaConsumer.java
+++ study/src/main/java/com/example/study/listener/SignupKafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.example.study.listener;
 
 import com.example.study.Service.EmailService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class SignupKafkaConsumer {
 
-    @Autowired
-    private EmailService emailService;
+   // @Autowired
+   // private EmailService emailService;
 
-    @KafkaListener(topics = "user-email", groupId = "study-group")
-    public void consume(String email) {
-        System.out.println("Kafka에서 메시지 수신: " + email);
-        emailService.sendConfirmationEmail(email);
+    @KafkaListener(topics = "user-email", groupId = "study-group", containerFactory = "kafkaConsumerFactory")
+    public void consume(ConsumerRecord<String, String> consumerRecord) {
+        System.out.println("Kafka에서 메시지 수신: " + consumerRecord.toString());
+        //emailService.sendConfirmationEmail(email);
     }
 }
